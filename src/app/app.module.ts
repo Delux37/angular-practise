8bd4c283240c9a069b6b2c65122be3d677099c65
Deluxe <giobibiluri.gb@gmail.com>
2021-09-12 22:05:18 +0400

    init folders for td-forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { UsersListComponent } from './basics/users-list/users-list.component';
 import { UsersCardComponent } from './basics/users-card/users-card.component';
 import { FormsModule } from '@angular/forms';
+import { SimpleFormComponent } from './forms/td-forms/simple-form/simple-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersListComponent,
-    UsersCardComponent
+    UsersCardComponent,
+    SimpleFormComponent
   ],
   imports: [
     BrowserModule,
