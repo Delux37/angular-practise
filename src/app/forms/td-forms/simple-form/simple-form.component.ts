08561ae4ee707d328dffa55e17d2d575105f170b
Deluxe <giobibiluri.gb@gmail.com>
2021-09-13 12:15:01 +0400

    Added simple validation messages

--- src/app/forms/td-forms/simple-form/simple-form.component.ts
+++ src/app/forms/td-forms/simple-form/simple-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+
 
 @Component({
   selector: 'app-simple-form',
@@ -11,5 +13,7 @@ export class SimpleFormComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onSubmit(form: NgForm){
+    console.log(form);
+  }
 }
