8e918939e6f96816ea9a6cdfced4f5a6c3ff97c0
Deluxe <giobibiluri.gb@gmail.com>
2021-09-13 12:35:07 +0400

    Reactive Form (RF) - init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppComponent } from './app.component';
 import { UsersListComponent } from './basics/users-list/users-list.component';
 import { UsersCardComponent } from './basics/users-card/users-card.component';
 import { FormsModule } from '@angular/forms';
-import { SimpleFormComponent } from './forms/td-forms/simple-form/simple-form.component';
+import { SimpleFormComponent } from './forms/td-forms/simple-form.component';
+import { ReactiveFormComponent } from './forms/reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersListComponent,
     UsersCardComponent,
-    SimpleFormComponent
+    SimpleFormComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
